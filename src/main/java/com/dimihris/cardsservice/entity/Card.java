8d7f6210a477c9672c62f477b0a91ae2c4bf8a1f
dimihris <hristo_dimitrov@flutterint.com>
2026-09-03 15:56:47 +0300

    feature: create BaseEntity and Card entity

---
+++ src/main/java/com/dimihris/cardsservice/entity/Card.java
@@ -0,0 +1,43 @@
+package com.dimihris.cardsservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "cards")
+@Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+public class Card extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Long cardId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private Integer totalLimit;
+
+    @Column(name = "amount_used")
+    private Integer amountUsed;
+
+    @Column(name = "available_amount")
+    private Integer availableAmount;
+}
