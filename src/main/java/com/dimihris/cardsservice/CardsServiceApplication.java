8ba7ba22f6cc9ea0f121a299075503be07b75714
dimihris <hristo_dimitrov@flutterint.com>
2026-09-11 16:16:56 +0300

    chode: create spring boot qa and prod profiles

--- src/main/java/com/dimihris/cardsservice/CardsServiceApplication.java
+++ src/main/java/com/dimihris/cardsservice/CardsServiceApplication.java
@@ -1,11 +1,14 @@
 package com.dimihris.cardsservice;
 
+import com.dimihris.cardsservice.dto.CardsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = CardsContactInfoDto.class)
 public class CardsServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dimihris/cardsservice/controller/CardController.java
+++ src/main/java/com/dimihris/cardsservice/controller/CardController.java
@@ -2,11 +2,14 @@ package com.dimihris.cardsservice.controller;
 
 import com.dimihris.cardsservice.constants.CardConstants;
 import com.dimihris.cardsservice.dto.CardDto;
+import com.dimihris.cardsservice.dto.CardsContactInfoDto;
 import com.dimihris.cardsservice.dto.response.ResponseDto;
 import com.dimihris.cardsservice.service.CardService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CardController {
 
-    private CardService cardService;
+    private final CardService cardService;
+
+    private final Environment environment;
+
+    private final CardsContactInfoDto cardsContactInfoDto;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createCard(
@@ -81,6 +92,27 @@ public class CardController {
                     .status(HttpStatus.EXPECTATION_FAILED)
                     .body(new ResponseDto(CardConstants.STATUS_417, CardConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @GetMapping("/contact-info")
+    public ResponseEntity<CardsContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardsContactInfoDto);
     }
 
 }
